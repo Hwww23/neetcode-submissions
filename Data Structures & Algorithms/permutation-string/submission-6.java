5996ca3536812c83b3ef50112b868f4cd7af04ae
Hwww23 <122247552+Hwww23@users.noreply.github.com>
2026-07-27 20:53:10 +0800

    Add: permutation-string - submission-6

---
+++ Data Structures & Algorithms/permutation-string/submission-6.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        if (s1.length() > s2.length()) return false;
+
+        HashMap<Character, Integer> need = new HashMap<>();
+        for (char c : s1.toCharArray()) {
+            need.put(c, need.getOrDefault(c, 0) + 1);
+        }
+        HashMap<Character, Integer> window = new HashMap<>(need);
+
+        int l = 0;
+        for (int r = 0; r < s2.length(); r++) {
+            if (window.get(s2.charAt(r)) == null) {
+                window = new HashMap<>(need);
+                l = r + 1;
+                continue;
+            } 
+
+            while (l < r && window.get(s2.charAt(r)) == 0) {
+                window.put(s2.charAt(l), window.get(s2.charAt(l)) + 1);
+                l++;
+            }
+
+            window.put(s2.charAt(r), window.get(s2.charAt(r)) - 1);
+
+            if (r - l + 1 == s1.length()) return true;
+        }
+
+        return false;
+    }
+}
