0c03c4da7ffcfc4647903fa65e200879cbcf2838
Hwww23 <122247552+Hwww23@users.noreply.github.com>
2026-08-01 09:14:07 +0800

    Add: word-break - submission-1

---
+++ Data Structures & Algorithms/word-break/submission-1.java
@@ -0,0 +1,25 @@
+class Solution {
+    Boolean[] memo;
+    public boolean wordBreak(String s, List<String> wordDict) {
+        memo = new Boolean[s.length()];
+        return dfs(0, s, wordDict);
+    }
+
+    private boolean dfs(int idx, String s, List<String> wordDict) {
+        if (idx == s.length()) {
+            return true;
+        }
+
+        if (memo[idx] != null) {
+            return memo[idx];
+        }
+
+        for (String word : wordDict) {
+            if (s.substring(idx).startsWith(word) && dfs(idx + word.length(), s, wordDict)) {
+                return memo[idx] = true;
+            }
+        }
+
+        return memo[idx] = false;
+    }
+}
