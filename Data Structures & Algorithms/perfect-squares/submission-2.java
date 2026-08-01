6821b2e03835e6223e8f868c47eb8326cfef8ced
Hwww23 <122247552+Hwww23@users.noreply.github.com>
2026-08-01 20:38:26 +0800

    Add: perfect-squares - submission-2

---
+++ Data Structures & Algorithms/perfect-squares/submission-2.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int numSquares(int n) {
+        List<Integer> nums = new ArrayList<>();
+        for (int i = 1; i * i <= n; i++) {
+            nums.add(i * i);
+        }
+
+        int[] memo = new int[n + 1];
+        Arrays.fill(memo, Integer.MAX_VALUE);
+        memo[0] = 0;
+
+        for (int i = 1; i <= n; i++) {
+            for (int num : nums) {
+                if (num > i) break;
+
+                memo[i] = Math.min(memo[i], 1 + memo[i - num]);
+            }
+        }
+
+        return memo[n];
+    }
+}
