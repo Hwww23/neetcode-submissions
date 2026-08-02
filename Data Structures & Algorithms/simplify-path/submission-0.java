888ddf009ff8d7d5dcbded9c86c89b8280525343
Hwww23 <122247552+Hwww23@users.noreply.github.com>
2026-08-03 07:34:12 +0800

    Add: simplify-path - submission-0

---
+++ Data Structures & Algorithms/simplify-path/submission-0.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String simplifyPath(String path) {
+        String[] arr = path.split("/");
+        Stack<String> stack = new Stack<>();
+
+        for (String s : arr) {
+            if (s.equals("") || s.equals(".")) {
+                continue;
+            } else if (s.equals("..")) {
+                if (!stack.isEmpty()) {
+                    stack.pop();
+                }
+            } else {
+                stack.push(s);
+            }
+        }
+
+        if (stack.isEmpty()) return "/";
+
+        String res = "";
+        while (!stack.isEmpty()) {
+            res = "/" + stack.pop() + res;
+        }
+
+        return res;
+    }
+}
