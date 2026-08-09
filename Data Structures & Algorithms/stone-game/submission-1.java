e3493b3c97543c16dc14b41224610549571c9260
Hwww23 <122247552+Hwww23@users.noreply.github.com>
2026-08-10 02:10:19 +0800

    Add: stone-game - submission-1

---
+++ Data Structures & Algorithms/stone-game/submission-1.java
@@ -0,0 +1,24 @@
+class Solution {
+    int[][] memo;
+
+    public boolean stoneGame(int[] piles) {
+        memo = new int[piles.length][piles.length];
+        for (int[] m : memo) {
+            Arrays.fill(m, -1);
+        }
+
+        return dfs(0, piles.length - 1, piles) > 0;
+    }
+
+    private int dfs(int i, int j, int[] piles) {
+        if (i == j) {
+            return piles[i];
+        }
+
+        if (memo[i][j] != -1) {
+            return memo[i][j];
+        }
+
+        return memo[i][j] = Math.max(piles[i] - dfs(i + 1, j, piles), piles[j] - dfs(i, j - 1, piles));
+    }
+}
