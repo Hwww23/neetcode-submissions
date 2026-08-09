68381bdbba8a0cace79f81268b749313101de66f
Hwww23 <122247552+Hwww23@users.noreply.github.com>
2026-08-10 02:23:48 +0800

    Add: pow-x-n - submission-0

---
+++ Data Structures & Algorithms/pow-x-n/submission-0.java
@@ -0,0 +1,21 @@
+class Solution {
+    public double myPow(double x, int n) {
+        double res = dfs(x, Math.abs(n));
+        return n < 0 ? 1 / res : res;
+    }
+
+    public double dfs(double x, int n) {
+        if (x == 0 || x == 1) return x;
+        
+        if (n == 0) {
+            return 1;
+        }
+
+        double half = dfs(x, n / 2);
+        if (n % 2 == 0) {
+            return half * half;
+        } else {
+            return half * half * x;
+        }
+    }
+}
