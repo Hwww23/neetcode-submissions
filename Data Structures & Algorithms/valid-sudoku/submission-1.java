7368ef4efeb2ce4dd2091cefd567bbcaf38f8c27
Hwww23 <122247552+Hwww23@users.noreply.github.com>
2026-08-16 03:07:36 +0800

    Add: valid-sudoku - submission-1

---
+++ Data Structures & Algorithms/valid-sudoku/submission-1.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        HashSet<Character>[] row = new HashSet[9];
+        HashSet<Character>[] col = new HashSet[9];
+        HashSet<Character>[] grid = new HashSet[9];
+
+        for (int i = 0; i < 9; i++) {
+            row[i] = new HashSet<>();
+            col[i] = new HashSet<>();
+            grid[i] = new HashSet<>();
+        }
+
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (Character.isDigit(board[i][j])) {
+                    if (!row[i].add(board[i][j]) 
+                        || !col[j].add(board[i][j])
+                        || !grid[i / 3 * 3 + j / 3].add(board[i][j])) {
+                        return false;
+                    }
+                }
+            }
+        }
+
+        return true;
+    }
+}
