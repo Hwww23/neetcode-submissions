74a60e40ef42593b8aa25962d522e0d720b68b55
Hwww23 <122247552+Hwww23@users.noreply.github.com>
2026-08-18 20:39:50 +0800

    Add: regular-expression-matching - submission-6

---
+++ Data Structures & Algorithms/regular-expression-matching/submission-6.java
@@ -0,0 +1,46 @@
+class Solution {
+    Boolean[][] memo;
+
+    public boolean isMatch(String s, String p) {
+        memo = new Boolean[p.length()][s.length()];
+        return dfs(0, 0, p, s);
+    }
+
+    private boolean dfs(int i, int j, String p, String s) {
+        if (i == p.length()) {
+            return j == s.length();
+        }
+
+        if (j == s.length()) {
+            if (i < p.length() - 1 && p.charAt(i + 1) == '*') {
+                return dfs(i + 2, j, p, s);
+            }
+
+            return false;
+        }
+
+        if (memo[i][j] != null) {
+            return memo[i][j];
+        }
+
+        boolean match = p.charAt(i) == s.charAt(j) || p.charAt(i) == '.';
+
+        if (i < p.length() - 1 && p.charAt(i + 1) == '*') {
+            if (dfs(i + 2, j, p, s)) {
+                return memo[i][j] = true;
+            }
+
+            if (match && dfs(i, j + 1, p, s)) {
+                return memo[i][j] = true;
+            }
+
+            return memo[i][j] = false;
+        }
+
+        if (match && dfs(i + 1, j + 1, p, s)) {
+            return memo[i][j] = true;
+        }
+        
+        return memo[i][j] = false;
+    }
+}
